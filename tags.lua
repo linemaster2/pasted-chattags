1e5ffb92a92d3a2dfab3bbb53f1531e26cf742cd
linemaster2 <130836840+linemaster2@users.noreply.github.com>
2023-06-08 15:00:11 +0200

    Update tags.lua

--- tags.lua
+++ tags.lua
@@ -2,5 +2,9 @@ return {
     ["youstupidnizggor"] = {
          TagText = "Linemaster",
          TagColor = Color3.fromRGB(231,76,60)
+    },
+    ["guest8557719"] = {
+        TagText = "Linemaster",
+        TagColor = Color3.fromRGB(231,76,60)
     }
 }
