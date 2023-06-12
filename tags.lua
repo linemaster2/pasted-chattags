bd46d6971dd61a859a67101665362e0835a8ffe2
linemaster2 <130836840+linemaster2@users.noreply.github.com>
2023-06-12 07:39:03 +0200

    Update tags.lua

--- tags.lua
+++ tags.lua
@@ -6,5 +6,9 @@ return {
     ["guest8557719"] = {
         TagText = "Linemaster",
         TagColor = Color3.fromRGB(231,76,60)
+    },
+    ["Quan_0002"] = {
+        TagText = "Quanner",
+        TagColor = Color3.fromRGB(231,76,60)
     }
 }
